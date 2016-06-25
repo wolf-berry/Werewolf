76aa9976ef3b9cfad12184ba281a50a95b07c736
LiShunyang <lishunyang@pku.edu.cn>
2016-06-25 19:20:22 +0800

    增加游戏room的相关component

--- src/client/main-app/features/game-room/GameRoom.vue
+++ src/client/main-app/features/game-room/GameRoom.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    GameRoom
-  </div>
+<div>
+  <focused-stream-window :stream="focusedStream"></focused-stream-window>
+  <unfocused-stream-windows class="game-room__unfocused-stream-windows" :streams="unfocusedStreams"></unfocused-stream-windows>
+</div>
 </template>
 
 <script>
+import FocusedStreamWindow from './FocusedStreamWindow';
+import UnfocusedStreamWindows from './UnfocusedStreamWindows';
+
 import {
   kill,
 } from '../../vuex/actions';
 
 export default {
   name: 'GameRoom',
+
+  components: {
+    FocusedStreamWindow,
+    UnfocusedStreamWindows,
+  },
 
   vuex: {
     actions: {
@@ -27,3 +36,14 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus">
+.game-rom
+  &__unfocused-stream-windows
+    position fixed
+    left 0
+    right 0
+    bottom 0
+    height 150px
+    border 1px solid red
+</style>
---
+++ src/client/main-app/features/game-room/UnfocusedStreamWindows.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div class="unfocused-stream-windows" v-for="stream in streams">
+    stream {{stream.getId()}}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'UnfocusedStreamWindows',
+
+  props: {
+    streams: Array,
+  },
+};
+</script>
+
+<style lang="stylus">
+.unfocused-stream-windows
+  width 100px
+  height 100px
+  border 1px solid grey
+</style>
