a6c82ee582b82203546e2e768261798a94436003
LiShunyang <lishunyang@pku.edu.cn>
2016-06-26 02:38:00 +0800

    引入demo代码

--- src/server/router.js
+++ src/server/router.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import game from './controllers/game';
 import games from './controllers/games';
@@ -7,9 +8,12 @@ import session from './controllers/session';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('main-app', { title: 'Express' });
-});
+// router.get('/', (req, res) => {
+//   res.render('main-app', { title: 'Express' });
+// });
+
+router.get('/game-hall', (req, res) => res.sendFile(path.join(__dirname, './public', 'index.html')));
+router.get('/game-room', (req, res) => res.sendFile(path.join(__dirname, './public', 'meeting.html')));
 
 /****** login ******/
 router.get('/login', (req, res) => res.render('login'));
