200b6f67d99cb1c72b2a191adac441e0d0978419
lianxh <xh0092@163.com>
2016-06-25 18:39:12 +0800

    mkdir controllers

--- src/server/router.js
+++ src/server/router.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import game from './controllers/game';
+
 const router = express.Router();
 
 /* GET home page. */
@@ -8,5 +10,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => res.render('login'));
+
+//router.put('/api/game/vote/:gameId', );
 
 export default router;
